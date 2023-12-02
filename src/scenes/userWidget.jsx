fb0306d6606e57b0c9f2f6cfd165aef9f1c59c2f
sri vardhan <srivardhan3566@gmail.com>
2023-12-02 16:43:12 +0530

    first commit

--- src/scenes/userWidget.jsx
+++ src/scenes/userWidget.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import {
     ManageAccountsOutlined,
-  EditOutlined,
   LocationOnOutlined,
   WorkOutlineOutlined,
 } from "@mui/icons-material";
